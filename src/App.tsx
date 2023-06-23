cc8ac9ae009a5ee41dd8eaefeabe90b570b91b3e
AleksTimofeev <timoha144a@gmail.com>
2023-06-23 23:45:53 +0300

    add router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './app.css';
+import {NavLink, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
       <h1>ONLINE-STORE</h1>
+      <Routes>
+        <Route path={''} element={<h2>Home page</h2>} />
+      </Routes>
     </div>
   );
 }
