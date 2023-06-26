af7c7cbb8257930cd177b061457f307fac52da44
AleksTimofeev <timoha144a@gmail.com>
2023-06-26 16:38:22 +0300

    change type registration thunk

--- src/components/authorization/authReducer.ts
+++ src/components/authorization/authReducer.ts
@@ -2,25 +2,31 @@ import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import {authApi, RegistrationType} from "../../api/authApi";
 import jwtDecode from "jwt-decode";
 
-export const registration = createAsyncThunk(
-  'auth/registration', async (arg: RegistrationType) => {
+export const registration = createAsyncThunk<UserType, RegistrationType, { rejectValue: string }>(
+  'auth/registration', async (arg, thunkAPI) => {
     try {
       const token = await authApi.registration(arg)
       localStorage.setItem('my token', token)
       return jwtDecode<UserType>(token)
     } catch (e) {
       console.log(e)
+      return thunkAPI.rejectWithValue('error')
     }
   }
 )
 
 const slice = createSlice({
   name: 'auth',
-  initialState: {} as AuthReducerType,
+  initialState: {
+    error: null
+  } as AuthReducerType,
   reducers: {},
   extraReducers: builder => {
     builder.addCase(registration.fulfilled, (state, action) => {
       if (action.payload) state.user = action.payload
+    })
+    builder.addCase(registration.rejected, (state, action) => {
+      if (action.payload) state.error = action.payload
     })
   }
 })
@@ -29,6 +35,7 @@ export const authReducer = slice.reducer
 
 export type AuthReducerType = {
   user: UserType
+  error: string | null
 }
 
 export type UserType = {
